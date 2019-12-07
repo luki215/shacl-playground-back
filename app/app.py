54d00d6c1f3a77f4c78ecaff7afac5f1332326c8
Lukas Brezina <lukas.brezina@coex.cz>
2019-12-07 21:58:03 +0000

    feat(app): return validator results

--- app/app.py
+++ app/app.py
@@ -3,9 +3,10 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import werkzeug
 from flask_cors import CORS
+from pyshacl import validate
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:4200'])
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
@@ -13,15 +14,19 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
     def post(self):
         parse = reqparse.RequestParser()
-        parse.add_argument('shacl_file', type=werkzeug.datastructures.FileStorage, location='files')
-        parse.add_argument('turtle_file', type=werkzeug.datastructures.FileStorage, location='files')
+        parse.add_argument('shapes_file', type=werkzeug.datastructures.FileStorage, location='files')
+        parse.add_argument('data_file', type=werkzeug.datastructures.FileStorage, location='files')
         args = parse.parse_args()
-        shaclFile = args['shacl_file']
-        shaclFile.save("shacl_file")
-        turtleFile = args['turtle_file']
-        turtleFile.save("turtle_file")
+        shaclFile = args['shapes_file']
+        shaclFile.save("tmp/shapes_file")
+        turtleFile = args['data_file']
+        turtleFile.save("tmp/data_file")
 
-        return {'ok': True}
+        r = validate("tmp/data_file", shacl_graph="tmp/shapes_file", data_graph_format="turtle", shacl_graph_format="turtle", debug=False)
+        conforms, results_graph, results_text = r
+
+
+        return {"conforms": conforms, "results_graph": str(results_graph.serialize(format="n3").decode("utf-8")), "results_text": results_text}
 
 api.add_resource(HelloWorld, '/')
 
